826ac953101ed4d0ecc58b6881f1f2610afb7a8e
myhg <myhg@myhgdemac-mini.local>
2016-01-13 10:47:19 +0800

    Initial Commit

---
+++ Segue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Segue
+//
+//  Created by myhg on 16/1/13.
+//  Copyright © 2016年 zhuming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Segue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Segue
+//
+//  Created by myhg on 16/1/13.
+//  Copyright © 2016年 zhuming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
